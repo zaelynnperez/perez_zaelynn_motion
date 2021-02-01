f97b3ed521effb6b7debd16189df59e0a7ddfac2
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-02-01 18:32:56 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log(input.acceleration(Dimension.X))
-    if (input.acceleration(Dimension.X) < 1 || input.acceleration(Dimension.X) > 1) {
+    if (input.acceleration(Dimension.X) < 1) {
         light.setAll(light.rgb(200, 165, 155))
     } else {
         light.clear()
