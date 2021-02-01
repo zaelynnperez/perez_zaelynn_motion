f3224d001cd467dd435ff84dfb71971f161b8661
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-02-01 18:04:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+while (true) {
+    console.log(input.rotation(Rotation.Pitch))
+    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) > 0) {
+        light.setAll(light.rgb(255, 0, 255))
+    } else {
+        light.clear()
+    }
+    
+}
