75224b2592e311aac9a5942f84e69e3049b7f8e8
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-02-01 18:04:31 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log(input.rotation(Rotation.Pitch))
-    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) > 0) {
+    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) < 0) {
         light.setAll(light.rgb(255, 0, 255))
     } else {
         light.clear()
